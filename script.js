d0e3360e8e1ee20fff6fb6c3e5fbb4f1a0e5ba2d
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-02 19:20:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateSizeValues() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+  
+    document.getElementById("widthValue").textContent = width;
+    document.getElementById("heightValue").textContent = height;
+  }
+
+  // Initial update of size values
+  updateSizeValues();
+
+  // Update size values on window resize
+  window.addEventListener("resize", updateSizeValues);
